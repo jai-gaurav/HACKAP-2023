f6dc84b0b5971c8c633f8806b52f6be226e6410b
Jai Gaurav <jaigaurav157@gmail.com>
2023-07-30 11:42:01 +0530

    updated plaeholder text near website footer
    Signed-off-by: Jai Gaurav <jaigaurav157@gmail.com>

--- Website/src/constants/index.js
+++ Website/src/constants/index.js
@@ -47,24 +47,24 @@ export const feedback = [
   {
     id: "feedback-1",
     content:
-      "Money is only a tool. It will take you wherever you wish, but it will not replace you as the driver.",
-    name: "Herman Jensen",
+      "Quote mera random dal de. Farak nahi badta behenchod!",
+    name: "Tarun Samala",
     title: "Founder & Leader",
     img: people01,
   },
   {
     id: "feedback-2",
     content:
-      "Money makes your life easier. If you're lucky to have it, you're lucky.",
-    name: "Steve Mark",
+      "Ore wa seigidesu!",
+    name: "Jai Gaurav",
     title: "Founder & Leader",
     img: people02,
   },
   {
     id: "feedback-3",
     content:
-      "It is usually people in the money business, finance, and international trade that are really rich.",
-    name: "Kenn Gallagher",
+      "Play, win, and make history!",
+    name: "Sagnik Sarkar",
     title: "Founder & Leader",
     img: people03,
   },
